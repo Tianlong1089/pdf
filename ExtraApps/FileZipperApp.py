9d4c9d62d1e90f801077320df655bd3884582a09
Tianlong1089 <carloscarrera@ciencias.unam.mx>
2023-05-11 15:02:30 -0600

    Create the File Compressor GUI without the zipper function, May 11

---
+++ ExtraApps/FileZipperApp.py
@@ -0,0 +1,22 @@
+'''
+This program uses GUI (Graphical User Interface)
+This Program opens a window to select the file or files to compress, extract the path file and finally
+make you choose the path were the zipped file will be created.
+'''
+import PySimpleGUI as sg
+
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
+
+label2 = sg.Text("Select destination folder: ")
+input2 = sg.Input()
+choose_button2 = sg.FilesBrowse("Choose")
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window("File Compressor",
+                   layout=[[label1,input1,choose_button1],[label2,input2,choose_button2],[compress_button]])
+
+window.read()
+window.close()

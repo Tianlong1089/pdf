7a9ef120e8e26b606df5951687f6c8ea9d5ce2a4
Tianlon1089 <carloscarrera@ciencias.unam.mx>
2023-05-12 13:34:28 -0600

    Implement the Edit button and fixing '\n' bugs, May 12

--- ToDo_List/gui.py
+++ ToDo_List/gui.py
@@ -4,18 +4,55 @@ import PySimpleGUI as sg
 label = sg. Text("Type in a to-do")
 input_box =  sg.InputText(tooltip="Enter todo", key = "todo")
 add_button = sg.Button("Add")
-window = sg.Window('My To-Do App', layout= [[label], [input_box,add_button]],font = ('Helvetica',20))
+
+edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values = functions.get_todos(), key = 'todos',enable_events=True, size=[45,10] )
+
+window = sg.Window('My To-Do App', layout= [[label], [input_box,add_button],[list_box,edit_button]],font = ('Helvetica',20))
 
 while True:
-    event, values = window.read()
-    match event:
-        case "Add":
-            todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
-        case sg.WINDOW_CLOSED:
-            break
+        event, values = window.read()
+        #print(1,values)
+        #print(2,values['todos'])
+        match event:
+            case "Add":
+                todos = functions.get_todos()
+                new_todo = values['todo'] + "\n"
+                todos.append(new_todo)
+                functions.write_todos(todos)
+
+                window['todos'].update(values=todos)
+            case "Edit":
+                try:
+                    todo_to_edit = values['todos'][0]
+                    if todo_to_edit[-2:] == '\n':
+                        new_todo = values['todo']
+                    else:
+                        new_todo = values['todo'] + '\n'
+                    if todo_to_edit[-4:] == '\n\n':
+                        new_todo = new_todo[:-2]
+                    print("new :", new_todo)
+                    print("todo_to_edit " ,todo_to_edit)
+
+
+                    todos=functions.get_todos()
+                    print("Todos: ", todos)
+                    index = todos.index(todo_to_edit)
+
+                    todos[index]= new_todo
+
+                    print(todos)
+                    functions.write_todos(todos)
+
+                    window['todos'].update(values=todos)
+                except IndexError:
+                    print('Choose a option while editing')
+                    continue
+
+            case 'todos':
+                window['todo'].update(value=values['todos'][0])
+            case sg.WINDOW_CLOSED:
+                break
 
 window.close()
 

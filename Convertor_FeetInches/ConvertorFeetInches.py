c3fd79502371837cc3fda1113d7c3ab85bdf996e
Tianlon1089 <carloscarrera@ciencias.unam.mx>
2023-05-15 15:11:43 -0600

    Add theme and exit button, May 15

--- Convertor_FeetInches/ConvertorFeetInches.py
+++ Convertor_FeetInches/ConvertorFeetInches.py
@@ -2,6 +2,8 @@ import PySimpleGUI as sg
 from FeetToMeters import feet_to_meters
 from FeetToMeters import inches_to_meters
 
+sg.theme("SystemDefaultForReal")
+
 label1 = sg.Text("Enter Feet:")
 input1 = sg.Input(key="feets")
 
@@ -10,8 +12,9 @@ input2 = sg.Input(key="inches")
 
 convertor_button = sg.Button("Convert")
 label3 = sg.Text(key = 'amount')
+exit_button = sg.Button("Exit")
 
-window = sg.Window("Convertor", layout = [[label1,input1],[label2,input2],[convertor_button,label3]])
+window = sg.Window("Convertor", layout = [[label1,input1],[label2,input2],[convertor_button,exit_button, label3]])
 while True:
     event,values=window.read()
 
@@ -22,6 +25,8 @@ while True:
             meters =  feet_to_meters(feets) + inches_to_meters(inches)
             string = f'{meters} m'
             window['amount'].update(value= string)
+        case "Exit":
+            break
         case sg.WINDOW_CLOSED:
             break
 window.close()

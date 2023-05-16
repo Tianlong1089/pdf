c4364ddc29429d0e9425ca3382d09a1ad642db04
Tianlon1089 <carloscarrera@ciencias.unam.mx>
2023-05-16 09:55:48 -0600

    Add comments for the functions, May 16

--- ToDo_List/functions.py
+++ ToDo_List/functions.py
@@ -1,10 +1,12 @@
 FilePath  = '/home/tianlong55/Downloads/PyCharm_Projects/ToDo_List/todos.txt'
 def get_todos(pathfile=FilePath):
+  """REad a text file and return the list of to-do items."""
   with open(pathfile,'r') as file_local:
     todos_local= file_local.readlines()
   return todos_local
 
 def write_todos(todos_loc,path_file=FilePath):
+  """ Write the to-do items list in the text file."""
   with open(path_file,'w') as file:
     file.writelines(todos_loc)
 

7e786bd7ac7969d138c177cb9595ba4d49f2192e
Tianlon1089 <carloscarrera@ciencias.unam.mx>
2023-05-16 13:28:27 -0600

    IMplement " Add new todo items" feature, May 16

--- ToDo_List/web.py
+++ ToDo_List/web.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import functions
+
 todos = functions.get_todos()
+def add_todo():
+    todo_local = st.session_state['new_todo'] +'\n'
+    todos.append(todo_local)
+    functions.write_todos(todos)
+
+
 
 st.title("My Todo App")
 st.subheader("This is a ToDo App made in Python.")
@@ -9,4 +16,6 @@ st.subheader("This is a ToDo App made in Python.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add new todo...")
+st.text_input(label="",placeholder="Add new todo...",on_change=add_todo,key='new_todo')
+
+#st.session_state
